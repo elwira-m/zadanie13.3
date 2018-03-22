a2c32484b32f516db1c576e540d732d69fae3159
elwira-m <36117564+elwira-m@users.noreply.github.com>
2018-03-22 03:08:41 +0100

    Create program.js

---
+++ program.js
@@ -0,0 +1,25 @@
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('readable', function() {
+			// tutaj treść tego co ma się wykonać w momencie odczytania wejścia.
+			// metoda .read() ma za zadanie odczytać co użytkownik podał na wejściu
+    var input = process.stdin.read();
+    if(input !== null) {
+			// teraz jest sens cokolwiek wyświetlać - użytkownik wpisał coś
+        var instruction = input.toString().trim();
+		switch (instruction) {
+			case 'exit':
+				process.stdout.write('Quitting app!');
+				process.exit();
+			break;
+			case 'language':
+				console.log(process.env.LANG);
+			break;
+			case 'version':
+				console.log(process.versions.node);
+			break;
+			default:
+				process.stderr.write('Wrong instruction!\n');
+		}
+	}
+})
